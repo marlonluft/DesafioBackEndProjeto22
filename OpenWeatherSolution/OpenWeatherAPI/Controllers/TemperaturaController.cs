e1683ddd24b0edabceb15e0079a682d6725fd05b
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-19 20:11:31 -0300

    Criação inicial do projeto

---
+++ OpenWeatherSolution/OpenWeatherAPI/Controllers/TemperaturaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OpenWeatherAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperaturaController : ControllerBase
+    {
+        private readonly ILogger<TemperaturaController> _logger;
+
+        public TemperaturaController(ILogger<TemperaturaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
