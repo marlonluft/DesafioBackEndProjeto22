dfe36f686ef89a2d436cdab2aeec226d94b80221
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-20 19:53:18 -0300

    Adicionado swagger

--- OpenWeatherSolution/src/OpenWeather.API/Startup.cs
+++ OpenWeatherSolution/src/OpenWeather.API/Startup.cs
@@ -3,12 +3,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using OpenWeather.API.Util;
+using System.IO;
 
 namespace OpenWeatherAPI
 {
     public class Startup
     {
+        private const string SwaggerTitle = "OpenWeather.API";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +30,22 @@ namespace OpenWeatherAPI
             services.ConfigurarHttpClient(Configuration);
             services.RegistrarInjecaoDependencia();
             services.ConfigurarTarefaSegundoPlano();
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = SwaggerTitle,
+                        Version = "v1"
+                    }
+                 );
+
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "OpenWeather.API.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +65,12 @@ namespace OpenWeatherAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", SwaggerTitle);
             });
         }
     }
