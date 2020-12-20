3698885cc8d6c43de85fff2ce7ce535a2cdc8230
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-20 18:40:42 -0300

    Preparação de service para comunicação http

--- OpenWeatherSolution/src/OpenWeather.Library/Services/OpenWeatherService.cs
+++ OpenWeatherSolution/src/OpenWeather.Library/Services/OpenWeatherService.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.Extensions.Options;
+using OpenWeather.Library.Constants;
 using OpenWeather.Library.Model;
 using OpenWeather.Library.Settings;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace OpenWeather.Library.Services
 {
-    public class OpenWeatherService : IOpenWeatherService
+    public class OpenWeatherService : IOpenWeatherService, IDisposable
     {
         private OpenWeatherSetting _openWeatherSetting { get; set; }
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly HttpClient _client;
 
-        public OpenWeatherService(IOptions<OpenWeatherSetting> openWeatherSetting)
+        public OpenWeatherService(IOptions<OpenWeatherSetting> openWeatherSetting, IHttpClientFactory clientFactory)
         {
             _openWeatherSetting = openWeatherSetting.Value;
+            _clientFactory = clientFactory;
+            _client = _clientFactory.CreateClient(HttpClientName.OPEN_WEATHER_MAP);
         }
 
         public Task<TemperaturaModel> ListarDadosMeteorologicosAtuais()
         {
             throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
         }
     }
 }
