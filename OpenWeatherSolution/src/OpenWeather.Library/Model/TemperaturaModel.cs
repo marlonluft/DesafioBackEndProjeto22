b763001c3a8b95a46cb38e53e834b2bb457b22b0
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-19 22:55:48 -0300

    Teste unitário campo descrição TemperaturaModel

---
+++ OpenWeatherSolution/src/OpenWeather.Library/Model/TemperaturaModel.cs
@@ -0,0 +1,39 @@
+﻿using OpenWeather.Library.Exceptions;
+using System.Linq;
+
+namespace OpenWeather.Library.Model
+{
+    public class TemperaturaModel
+    {
+        public TemperaturaModel(string cidade, string[] descricoes, float sensacaoTermica, int humidade, float temperatua, float temperatuaMaxima, float temperatuaMinima)
+        {
+            this.Cidade = cidade;
+            this.Descricoes = descricoes;
+            this.SensacaoTermica = sensacaoTermica;
+            this.Humidade = humidade;
+            this.Temperatua = temperatua;
+            this.TemperatuaMaxima = temperatuaMaxima;
+            this.TemperatuaMinima = temperatuaMinima;
+        }
+
+        public string Cidade { get; private set; }
+        public string[] Descricoes { get; private set; }
+        public float SensacaoTermica { get; private set; }
+        public int Humidade { get; private set; }
+        public float Temperatua { get; private set; }
+        public float TemperatuaMaxima { get; private set; }
+        public float TemperatuaMinima { get; private set; }
+
+        public void Validar()
+        {
+            if (Descricoes == null || Descricoes.Length == 0)
+            {
+                throw new OpenWeatherException("Nenhuma descrição informada");
+            }
+            else if (Descricoes.Any(x => string.IsNullOrWhiteSpace(x)))
+            {
+                throw new OpenWeatherException("Nenhuma descrição válida informada");
+            }
+        }
+    }
+}
---
+++ OpenWeatherSolution/tests/OpenWeather.Library.Tests/Model/TemperaturaTest.cs
@@ -0,0 +1,49 @@
+﻿using OpenWeather.Library.Exceptions;
+using OpenWeather.Library.Model;
+using Xunit;
+
+namespace OpenWeather.Library.Tests.Model
+{
+    public class TemperaturaTest
+    {
+        [Fact(DisplayName = "Preencher Descrição nula")]
+        [Trait("Model", "TemperaturaModel")]
+        public void PreencherDescrcaioNula_DeveRetornarException()
+        {
+            // Arrange
+            var temperatura = new TemperaturaModel("Cidade", null, 0, 0, 0, 0, 0);
+
+            // Act & Assert
+            Assert.Throws<OpenWeatherException>(() => temperatura.Validar());
+        }
+
+        [Fact(DisplayName = "Preencher Descrição sem registros")]
+        [Trait("Model", "TemperaturaModel")]
+        public void PreencherDescrcaioSemRegistros_DeveRetornarException()
+        {
+            // Arrange
+            var descricoes = new string[0];
+
+            var temperatura = new TemperaturaModel("Cidade", descricoes, 0, 0, 0, 0, 0);
+
+            // Act & Assert
+            Assert.Throws<OpenWeatherException>(() => temperatura.Validar());
+        }
+
+        [Fact(DisplayName = "Preencher Descrição vazia")]
+        [Trait("Model", "TemperaturaModel")]
+        public void PreencherDescrcaioVazia_DeveRetornarException()
+        {
+            // Arrange
+            var descricoes = new string[1]
+            {
+                string.Empty
+            };
+
+            var temperatura = new TemperaturaModel("Cidade", descricoes, 0, 0, 0, 0, 0);
+
+            // Act & Assert
+            Assert.Throws<OpenWeatherException>(() => temperatura.Validar());
+        }
+    }
+}
