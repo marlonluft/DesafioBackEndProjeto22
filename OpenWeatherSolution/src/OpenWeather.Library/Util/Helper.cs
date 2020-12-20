5d0d767bc27f66d1ac8b72ed0ecd4e93b0b4f4a9
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-20 20:50:14 -0300

    Criação de classe com método de auxilio

---
+++ OpenWeatherSolution/src/OpenWeather.Library/Util/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace OpenWeather.Library.Util
+{
+    public class Helper
+    {
+        public static string NormalizarString(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return valor;
+            }
+
+            return CultureInfo.CreateSpecificCulture("pt-BR").TextInfo.ToTitleCase(valor.Trim());
+        }
+
+        public static bool CompararIgnorandoAcentos(string texto1, string texto2)
+        {
+            return string.Compare(
+              RemoverAcentos(texto1), RemoverAcentos(texto2), StringComparison.InvariantCultureIgnoreCase) == 0;
+        }
+
+        private static string RemoverAcentos(string texto)
+        {
+            Encoding destEncoding = Encoding.GetEncoding("iso-8859-8");
+
+            return destEncoding.GetString(
+              Encoding.Convert(Encoding.UTF8, destEncoding, Encoding.UTF8.GetBytes(texto)));
+        }
+    }
+}
