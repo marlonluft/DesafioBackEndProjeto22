1cdeec7fa6258ba0552e85ef236d1915dde885f7
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-20 20:22:54 -0300

    Documentação de Controller swagger

--- OpenWeatherSolution/src/OpenWeather.API/Controllers/TemperaturaController.cs
+++ OpenWeatherSolution/src/OpenWeather.API/Controllers/TemperaturaController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OpenWeather.API.ViewModel;
+using System;
+using System.Collections.Generic;
 
 namespace OpenWeatherAPI.Controllers
 {
+    /// <summary>
+    /// Ponto de consultade de dados referentes a temperatura de uma ou mais regiões em unidade métrica
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class TemperaturaController : ControllerBase
@@ -14,9 +20,23 @@ namespace OpenWeatherAPI.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Consulta de dados de temperatura
+        /// </summary>
+        /// <param name="cidade">Cidades para consulta de temperatura (São Paulo, Florianópolis, Rio De janeiro)</param>
+        /// <param name="dataInicio">Data inicial do histórico de pesquisa</param>
+        /// <param name="dataFim">Data fim do histórico de pesquisa</param>
+        /// <response code="200">Histórico de temperaturas das cidade/periodo informados</response>
+        /// <response code="400">Dado de consulta inválido</response>
+        /// <response code="500">Falha interna, favor tentar novamente mais tarde</response>
         [HttpGet]
-        public IActionResult Get()
+        [ProducesResponseType(typeof(List<TemperaturaViewModel>), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(500)]
+        public IActionResult Get(string cidade, DateTime dataInicio, DateTime dataFim)
         {
+
+
             return Ok();
         }
     }
