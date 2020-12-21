9d45e792a70a24087fdf0489b2edcd7565f4e33a
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-12-20 22:26:33 -0300

    Testes unitários para validação de cache

--- OpenWeatherSolution/src/OpenWeather.Library/Util/Cache.cs
+++ OpenWeatherSolution/src/OpenWeather.Library/Util/Cache.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using OpenWeather.Library.Exceptions;
 using System;
 
 namespace OpenWeather.Library.Util
@@ -14,11 +15,21 @@ namespace OpenWeather.Library.Util
 
         public bool ConsultarCache<T>(string chave, out T objeto)
         {
+            if (string.IsNullOrWhiteSpace(chave))
+            {
+                throw new OpenWeatherException("Chave informada para o cache é inválida");
+            }
+
             return _memoryCache.TryGetValue(chave, out objeto);
         }
 
         public void GravarCache<T>(string chave, T objeto, TimeSpan expiracao)
         {
+            if (string.IsNullOrWhiteSpace(chave))
+            {
+                throw new OpenWeatherException("Chave informada para o cache é inválida");
+            }
+
             var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(expiracao);
             _memoryCache.Set(chave, objeto, cacheEntryOptions);
         }
---
+++ OpenWeatherSolution/tests/OpenWeather.Library.Tests/Util/CacheTest.cs
@@ -0,0 +1,74 @@
+﻿using OpenWeather.Library.Exceptions;
+using OpenWeather.Library.Util;
+using System;
+using Xunit;
+
+namespace OpenWeather.Library.Tests.Util
+{
+    public class CacheTest
+    {
+        private ICache _cache;
+
+        public CacheTest(ICache cache)
+        {
+            _cache = cache;
+        }
+
+        [Fact(DisplayName = "Gravação e consulta iguais")]
+        [Trait("Util", "Cache")]
+        public void GravacaoConsulta_DeveSerIguais()
+        {
+            // Arrange
+            var dadosParaGuardar = "Teste de Cache";
+
+            // Act
+            _cache.GravarCache("CacheTeste", dadosParaGuardar, TimeSpan.FromMinutes(1));
+            _cache.ConsultarCache("CacheTeste", out string resultado);
+
+            // Assert
+            Assert.Equal(dadosParaGuardar, resultado);
+        }
+
+        [Fact(DisplayName = "Chave de cache vazia")]
+        [Trait("Util", "Cache")]
+        public void ChaveCacheVazia_DeveLancarExcecao()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<OpenWeatherException>(() => _cache.GravarCache("", "Valor", TimeSpan.FromMilliseconds(1)));
+        }
+
+        [Fact(DisplayName = "Chave de cache nula")]
+        [Trait("Util", "Cache")]
+        public void ChaveCacheNula_DeveLancarExcecao()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<OpenWeatherException>(() => _cache.GravarCache(null, "Valor", TimeSpan.FromMilliseconds(1)));
+        }
+
+        [Fact(DisplayName = "Alteração de cache deve ocorrer")]
+        [Trait("Util", "Cache")]
+        public void AlteracaoCache_DeveOcorrer()
+        {
+            // Arrange
+            var dadosParaGuardar = "Teste de Cache";
+            var dadosParaAlterar = "111";
+
+            // Act
+            _cache.GravarCache("CacheTeste", dadosParaGuardar, TimeSpan.FromMinutes(1));
+            _cache.GravarCache("CacheTeste", dadosParaAlterar, TimeSpan.FromMinutes(1));
+
+            _cache.ConsultarCache("CacheTeste", out string resultado);
+
+            // Assert
+            Assert.Equal(dadosParaAlterar, resultado);
+        }
+
+        [Fact(DisplayName = "Consulta com chave nula")]
+        [Trait("Util", "Cache")]
+        public void ConsultaChaveNula_DeveLancarExcecao()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<OpenWeatherException>(() => _cache.ConsultarCache(null, out string teste));
+        }
+    }
+}
